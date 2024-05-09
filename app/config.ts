39d69f2a134df874083ee0f9271c7710635ffafe
jvaleskadevs <vmjune2019@gmail.com>
2024-05-09 14:42:53 +0200

    newVersion01

--- app/config.ts
+++ app/config.ts
@@ -1,3 +1,3 @@
 const LOCALHOST = 'http://localhost:3000';
-const DOMAIN_URL = 'https://words-frame.vercel.app';
+const DOMAIN_URL = 'https://words-frame-git-newversion01-jvaleskadevs-projects.vercel.app';
 export const URL: string = process.env.NODE_ENV === 'development' ? LOCALHOST : DOMAIN_URL;
