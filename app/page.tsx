49dc21733892b5629c94becc5629e9c73dbdb148
jvaleskadevs <vmjune2019@gmail.com>
2024-07-08 22:20:30 +0200

    game56

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { URL } from './config';
 
 const title = 'Words Game';
 const description = 'Words Game by J.';
-const image = `${URL}/game_55.jpg`;
+const image = `${URL}/game_56.jpg`;
 
 const frameMetadata = getFrameMetadata({
   buttons: [
